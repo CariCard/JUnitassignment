4d89f21a074f9ea631b464e2f1cfdbbc45958fbd
CariCard <112133987+CariCard@users.noreply.github.com>
2022-10-18 10:20:31 -0400

    Add files via upload

---
+++ wordTesting.java
@@ -0,0 +1,12 @@
+package jUnit;
+import org.junit.Test;
+import org.junit.Assert.*;
+
+public class wordTesting {
+
+	public void test () {
+		wordOccurances test = new wordOccurances ();
+		
+	}
+
+}
