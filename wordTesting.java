b8b8dd220b8a4962f2336a0edcaf4388c70e3cd9
CariCard <112133987+CariCard@users.noreply.github.com>
2022-10-18 10:47:36 -0400

    Update wordTesting.java

--- wordTesting.java
+++ wordTesting.java
@@ -1,12 +1,20 @@
 package jUnit;
-import org.junit.Test;
-import org.junit.Assert.*;
+import java.util.concurrent.Future;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
 public class wordTesting {
 
-	public void test () {
-		wordOccurances test = new wordOccurances ();
-		
-	}
+	  public static void main(String[] args) {
+	      Result result = JUnitCore.runClasses(wordOccurances.class);
+			
+	      for (Future failure : result.getFailures()) {
+	         System.out.println(failure.toString());
+	      }
+			
+	      System.out.println(result.wasSuccessful());
+	   }
+	}  
 
-}
